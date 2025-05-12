4248e44a6c78c20ffbccd3a5a77b69fbef9ec0a0
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-05-12 20:33:07 +0900

    feat: Add CreatePlan
    일정생성 기능 추가

--- src/main/java/com/example/todoplanner/repository/PlanRepository.java
+++ src/main/java/com/example/todoplanner/repository/PlanRepository.java
@@ -1,4 +1,8 @@
 package com.example.todoplanner.repository;
 
+import com.example.todoplanner.dto.PlanResponseDto;
+import com.example.todoplanner.entity.Plan;
+
 public interface PlanRepository {
+    PlanResponseDto savePlan(Plan plan);
 }
